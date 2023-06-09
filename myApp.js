54be342ba969678de3a235296874b8e260eca8d2
Jablonies <miguel.palomares@obf.ateneo.edu>
2023-06-09 13:02:45 +0800

    adjusted syntax

--- myApp.js
+++ myApp.js
@@ -13,12 +13,14 @@ app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 
-app.use(helmet.contentSecurityPolicy({
-  directives:{
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
     defaultSrc: ["'self'"],
-    scriptSrc: ["'self'","'trusted-cdn.com'"]
-  }
-}));
+    scriptSrc: ["'self'",'trusted-cdn.com']
+    } 
+  })
+);
 
 
 
